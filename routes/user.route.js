106de877402ebe19ae7bc12357739d7a16e2a282
MPV-Code <ccmlnm1200@gmail.com>
2020-09-16 10:47:11 +0700

    'Middeware'

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,7 +2,8 @@ var express = require('express');
 var shortid = require('shortid');
 
 var db = require('../db');
-var controller = require('../controllers/user.controller.js')
+var controller = require('../controllers/user.controller.js');
+var validate = require('../validate/user.validate.js');
 
 var router = express.Router();
 
@@ -14,6 +15,6 @@ router.get('/create', controller.create);
 
 router.get('/:id', controller.get);
 
-router.post('/create', controller.postCreate);
+router.post('/create', validate.postCreate, controller.postCreate);
 
 module.exports = router;
